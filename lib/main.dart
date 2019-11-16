0b89e08d96b378bb13f8e172839e097b0e49106f
mirxtrem-apps <mirxtrem.apps@gmail.com>
2019-11-16 00:23:26 -0500

    initial commit - spalsh page

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tractiv/pages/intro_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: '/',
+      routes: <String, WidgetBuilder> {
+        '/': (BuildContext context) => IntroPage()
+      },
+    );
+  }
+}
---
+++ lib/theme/custom_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  // Primarios
+  final sage = Color.fromRGBO(135, 188, 191, 1);
+  final rust = Color.fromRGBO(217, 125, 84, 1);
+  final drab = Color.fromRGBO(50, 71, 85, 1);
+
+  // Secundario
+  final ice = Color.fromRGBO(240, 243, 244, 1);
+  final fossil = Color.fromRGBO(200, 209, 211, 1);
+  final sand = Color.fromRGBO(185, 176, 162, 1);
+  // Tipografia
+  final onyx = Color.fromRGBO(27, 28, 32, 1);
+  final slate = Color.fromRGBO(110, 140, 160, 1);
+  final snow = Colors.white;
+}
